f765d70d1c248cced6bc49e6131bb8e51516ab8d
Zendard <thijsmuylaert@hotmail.com>
2024-06-18 15:02:56 +0200

    Add Simple login form

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use rocket::{fs::NamedFile, get, launch, routes};
+
+#[get("/<file>")]
+async fn serve_page(file: &str) -> Option<NamedFile> {
+    NamedFile::open(format!("views/{}.html", file)).await.ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![serve_page])
+}
