bc08d0f9f6265fe746850e947cabf7ad9d1f9968
Zendard <thijsmuylaert@hotmail.com>
2024-06-30 21:02:19 +0200

    Switch to surrealdb

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use sea_orm::entity::prelude::*;
+use rocket::{
+    form::{Form, FromForm}, post
+};
+use surrealdb::engine::remote::ws;
+use surrealdb::Surreal;
 
 struct User {
     id: u32,
@@ -8,8 +12,6 @@ struct User {
     rooms: Vec<Room>,
 }
 
-impl User {}
-
 struct Room {
     id: u32,
     messages: Vec<Message>,
@@ -18,4 +20,42 @@ struct Room {
 struct Message {
     id: u32,
     content: String,
+}
+
+#[derive(FromForm)]
+pub struct LoginForm {
+    email: String,
+    password: String,
+}
+
+#[post("/login/checkuser", data = "<login_form>")]
+pub async fn check_user(login_form: Form<LoginForm>) -> rocket::response::Redirect {
+    let id = get_user(login_form.email.to_owned(),login_form.password.to_owned()).await;
+    match id {
+    None => rocket::response::Redirect::to("/register"),
+    Some(_id) => rocket::response::Redirect::to("/"),
+}
+}
+
+pub async fn get_user(email:String,password:String)->Option<String>{
+    let db = connect_to_db().await?;
+    dbg!(db.query("SELECT * FROM users").await.unwrap());
+    let mut id = db
+        .query("SELECT id FROM users WHERE email=$email AND password=$password")
+        .bind((email,password)).await.unwrap();
+    dbg!(&id);
+
+    id.take(0).unwrap()
+}
+
+pub async fn connect_to_db() -> Option<Surreal<ws::Client>> {
+    let db = Surreal::new::<ws::Ws>("localhost:5000").await.ok()?;
+
+    db.signin(surrealdb::opt::auth::Root{
+        username: "root",
+        password: env!("DB_PASSWORD"),
+    }).await.ok()?;
+
+    db.use_ns("tobychat").use_db("main").await.ok()?;
+    Some(db)
 }
